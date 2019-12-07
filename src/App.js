c5505ee8aa40749da8f935291599d38c1d7e2441
Leonardo Mello Silvestri <Leonardomscontato@gmail.com>
2019-12-07 20:57:09 -0200

    Adding Floating component to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import Floating from './Components/Floating/Floating';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import Characters from './Components/Characters/Characters';
@@ -12,6 +13,7 @@ function App() {
     <div className="App indigo lighten-2 white-text">
       <BrowserRouter>
         <Navbar />
+        <Floating />
         <div>
           <Route exact path="/" component={() => <Home />} />
           <Route path="/characters" component={() => <Characters />} />
