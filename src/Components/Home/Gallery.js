8940176a16bed5765fe22411d4ffc87cb0d48e31
Leonardo Mello Silvestri <Leonardomscontato@gmail.com>
2019-12-07 20:08:38 -0200

    CSS for my Characters component

---
+++ src/Components/Home/Gallery.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <>
+      <section>
+        <div className="gallery custom-container center">
+          <h3 className="">Gallery</h3>
+          <div className="row">
+            <div className="col s12 m6 l4">
+              <img className="materialboxed responsive-img" src={require('../../img/gallery1.jpg')} />
+            </div>
+            <div className="col s12 m6 l4">
+              <img className="materialboxed responsive-img" src={require('../../img/gallery2.jpg')} />
+            </div>
+            <div className="col s12 m6 l4">
+              <img className="materialboxed responsive-img" src={require('../../img/gallery3.jpg')} />
+            </div>
+            <div className="col s12 m6 l4">
+              <img className="materialboxed responsive-img" src={require('../../img/gallery5.jpg')} />
+            </div>
+            <div className="col s12 m6 l4">
+              <img className="materialboxed responsive-img" src={require('../../img/gallery5.jpg')} />
+            </div>
+            <div className="col s12 m6 l4">
+              <img className="materialboxed responsive-img" src={require('../../img/gallery6.jpg')} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Gallery;
